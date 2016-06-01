0bab34eaf8b2002790aca4409ca88634bb208d3d
Maciek Swat <maciekswat@gmail.com>
2016-06-01 17:11:47 -0400

    commented out Ipython extension for rst

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,8 @@ extensions = [
     'sphinx.ext.extlinks',
     'sphinx.ext.mathjax',
     # 'numpydoc',
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
+    # 'IPython.sphinxext.ipython_directive',
+    # 'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 
